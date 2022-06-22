43df1b9e094bca58a3ef9bc0a59d4aa21cb3e2b1
Morten Piibeleht <morten.piibeleht@gmail.com>
2022-06-22 15:01:27 +1200

    Add pip

--- src/DocumenterMarkdown.jl
+++ src/DocumenterMarkdown.jl
@@ -13,4 +13,28 @@ Selectors.order(::Type{MarkdownFormat}) = 1.0
 Selectors.matcher(::Type{MarkdownFormat}, fmt, _) = isa(fmt, Markdown)
 Selectors.runner(::Type{MarkdownFormat}, fmt, doc) = render(doc, fmt)
 
+# This is from the old Deps module:
+
+"""
+    pip(deps...)
+
+Installs (as non-root user) all python packages listed in `deps`.
+
+# Examples
+
+```julia
+using Documenter
+
+makedocs(
+    # ...
+)
+
+deploydocs(
+    deps = Deps.pip("pygments", "mkdocs", "mkdocs-material"),
+    # ...
+)
+```
+"""
+pip(deps...) = () -> map(dep -> run(`pip install --user $(dep)`), deps)
+
 end # module
